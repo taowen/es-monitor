95af9cdf946c00a8626667b590f928dbd1475346
Tao Wen <taowen@gmail.com>
2016-02-04 00:15:37 +0800

    odin plugin take tags

--- es_monitor.py
+++ es_monitor.py
@@ -10,13 +10,23 @@ import json
 import time
 import base64
 
+
+def to_str(val):
+    if isinstance(val, basestring):
+        return val
+    else:
+        return str(val)
+
+
 if __name__ == "__main__":
     metric_name = sys.argv[1]
     sql = base64.b64decode(sys.argv[2])
     rows = es_query.execute_sql(sql)
     ts = int(time.time())
     datapoints = []
-    for datapoint in rows:
+    for row in rows or []:
+        datapoint = {'value': row.pop('value', 0)}
+        datapoint['tags'] = {to_str(k): to_str(v) for k, v in row.iteritems()}
         datapoint['name'] = metric_name
         datapoint['timestamp'] = ts
         datapoints.append(datapoint)
