b1dd8c1aa16d3c83c743b844370f46552517d293
Tao Wen <taowen@gmail.com>
2016-02-14 23:23:14 +0800

    import sample data

--- sample/symbol.py
+++ sample/symbol.py
@@ -1,13 +1,58 @@
 import urllib2
 import json
 import sys
+import csv
 
 
 def main():
     create_index_template()
     delete_index()
     create_index()
-    
+    symbols = read_symbols()
+    request = urllib2.Request('http://localhost:9200/_bulk', data='\n'.join(bluk_import_lines(symbols)))
+    response = urllib2.urlopen(request).read()
+    print(response)
+
+
+def bluk_import_lines(symbols):
+    for symbol in symbols:
+        yield json.dumps({'index': {'_index': 'symbol', '_type': 'symbol'}})
+        yield json.dumps(symbol)
+
+
+def read_symbols():
+    for exchange in ['nasdaq', 'nyse', 'nyse mkt']:
+        with open('%s.csv' % exchange) as f:
+            f.readline()
+            for symbol in csv.DictReader(f,
+                                         fieldnames=['symbol', 'name', 'last_sale', 'market_cap', 'ipo_year', 'sector',
+                                                     'industry']):
+                symbol.pop(None, None)
+                symbol['exchange'] = 'nasdaq'
+                if 'n/a' == symbol['ipo_year']:
+                    symbol['ipo_year'] = None
+                else:
+                    symbol['ipo_year'] = int(symbol['ipo_year'])
+                if 'n/a' == symbol['last_sale']:
+                    symbol['last_sale'] = None
+                else:
+                    symbol['last_sale'] = int(float(symbol['last_sale']) * 100)
+                if 'n/a' == symbol['market_cap']:
+                    symbol['market_cap'] = None
+                elif '0' == symbol['market_cap']:
+                    symbol['market_cap'] = 0
+                else:
+                    try:
+                        symbol['market_cap'] = long(float(symbol['market_cap'][1:]))
+                    except:
+                        unit = symbol['market_cap'][-1]
+                        if 'M' == unit:
+                            symbol['market_cap'] = long(float(symbol['market_cap'][1:-1]) * 1000L * 1000L)
+                        elif 'B' == unit:
+                            symbol['market_cap'] = long(float(symbol['market_cap'][1:-1]) * 1000L * 1000L * 1000L)
+                        else:
+                            raise Exception('unexpected unit: %s' % symbol['market_cap'])
+                yield symbol
 
 
 def delete_index():
